4b8fef9590720e68e68826d97483764c63ebfaaa
noku-lab <61013631+noku-lab@users.noreply.github.com>
2020-05-14 17:59:10 +0200

    changes imagelink

--- src/Components/ImageLinkForm/ImageLinkForm.js
+++ src/Components/ImageLinkForm/ImageLinkForm.js
@@ -6,7 +6,7 @@ const ImageLinkForm = ({ onInputChange, onButtonSubmit }) => {
     return (
         <div>
             <p className ='f3'>
-                {'Paster a url of a picture and this app will detect the face'}
+                {'Paste a url of a picture and this app will detect the face'}
             </p>
             <div className = 'center'>
                 <div className ="form center pa4 br3 shadow-5">
