cc1be2293ae2f36f1de64ac69ae96056eebbc083
noku-lab <61013631+noku-lab@users.noreply.github.com>
2020-05-14 21:11:11 +0200

    Updates

---
+++ precache-manifest.ceb2177a758d2a1db4a09a8ef4b4b654.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98ac852ceb1e3218b7183ad0f0dfa9ad",
+    "url": "/nokusmart/index.html"
+  },
+  {
+    "revision": "b2d339897557e40b056d",
+    "url": "/nokusmart/static/css/2.66b7f74f.chunk.css"
+  },
+  {
+    "revision": "05a1a15eae2220fc404b",
+    "url": "/nokusmart/static/css/main.6403558b.chunk.css"
+  },
+  {
+    "revision": "b2d339897557e40b056d",
+    "url": "/nokusmart/static/js/2.2c5667cf.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/nokusmart/static/js/2.2c5667cf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "05a1a15eae2220fc404b",
+    "url": "/nokusmart/static/js/main.8d3f980e.chunk.js"
+  },
+  {
+    "revision": "f2f2b94c6fcc1b8014b0",
+    "url": "/nokusmart/static/js/runtime-main.1207a3ef.js"
+  }
+]);
